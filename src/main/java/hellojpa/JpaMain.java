112150b1b77ccca9421cbc093151e88b60bd78f4
rnfkaus8 <kth4350@gmail.com>
2022-03-01 19:55:47 +0900

    같은 영속 엔티티롤 조회 시 동일한 객체 반환(영속 엔티티의 동일성 보장)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,6 +21,8 @@ public class JpaMain {
             Member findMember1 = em.find(Member.class, 4L);
             Member findMember2 = em.find(Member.class, 4L);
 
+            System.out.println("result = " + (findMember1 == findMember2));
+
             tx.commit();
         } catch (Exception e){
             tx.rollback();
