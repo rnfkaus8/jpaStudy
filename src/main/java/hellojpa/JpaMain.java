cadcdfca81d55cf8bd4750978f554094a8d49e87
rnfkaus8 <kth4350@gmail.com>
2022-03-08 21:24:27 +0900

    양방향 연관관계 규칙  - 객체의 두 관계 중 하나를 연관관계의 주인으로 지정  - 연관관계의 주인만이 외래키를 관리(등록, 수정)  - 주인이 아닌 쪽은 읽기만 가능  - 주인은 mappedBy 사용 X  - 주인이 아니면 mappedBy 속성으로 주인 지정  - 디비 테이블에 외래키가 있는 곳이 N (양방향 연관관계 주인)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,26 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
 
+            //저장
+            Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setUsername("Member1");
-            member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m: members) {
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
             tx.commit();
         } catch (Exception e){
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -14,8 +14,12 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -33,11 +37,11 @@ public class Member {
         this.username = username;
     }
 
-    public Long getTeamId() {
-        return teamId;
+    public Team getTeam() {
+        return team;
     }
 
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
