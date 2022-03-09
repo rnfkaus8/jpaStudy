71a886104ebb68d01d48194e80fff55863409218
rnfkaus8 <kth4350@gmail.com>
2022-03-09 21:37:28 +0900

    양방향 연관관계 매핑 시 가장 많이 하는 실수  - 연관관계의 주인에 값을 입력하지 않을 경우

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,24 +18,21 @@ public class JpaMain {
         try {
 
             //저장
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
 
             Member member = new Member();
             member.setUsername("Member1");
-            member.setTeam(team);
             em.persist(member);
+
+            Team team = new Team();
+            team.setName("TeamA");
+            team.getMembers().add(member);
+            em.persist(team);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            List<Member> members = findMember.getTeam().getMembers();
 
-            for (Member m: members) {
-                System.out.println("m.getUsername() = " + m.getUsername());
-            }
+
             tx.commit();
         } catch (Exception e){
             tx.rollback();
