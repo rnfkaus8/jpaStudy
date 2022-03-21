528dc2076e5dfddb8fca6d52b79b390f61b44e55
rnfkaus8 <kth4350@gmail.com>
2022-03-21 21:33:28 +0900

    값 타입 컬렉션  - 값 타입을 하나 이상 저장할 때 사용  - @ElementCollection, @CollectionTable 사용  - 데이터베이스는 컬렉션을 같은 테이블에 저장할 수 없다  - 컬렉션을 저장하기 위해서는 별도의 테이블이 필요함  - 정말 단순한 상황이 아니면 값 타입 컬렉션을 승격해서 엔티티로 만드는게 좋음

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,9 +2,7 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Entity(name = "BASIC_MEMBER")
 public class Member{
@@ -24,6 +22,22 @@ public class Member{
     @Embedded
     private Address homeAddress;
 
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD"
+            , joinColumns = @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS"
+//            , joinColumns = @JoinColumn(name = "MEMBER_ID")
+//    )
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -55,5 +69,21 @@ public class Member{
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
