ccc3a5215a66b0c268d296922e1d5a832a9bdc3c
rnfkaus8 <kth4350@gmail.com>
2022-03-22 21:08:07 +0900

    JPQL  - 테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리  - SQL을 추상화해서 특정 데이터베이스 SQL에 종속되지 않음  - 객체 지향 SQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,47 +17,13 @@ public class JpaMain {
         tx.begin();
 
         try {
+            List<Member> memberList = em.createQuery(
+                    "select m From BASIC_MEMBER m where m.username like '%kim%'", Member.class
+            ).getResultList();
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+            for (Member member : memberList) {
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "20000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "20000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("==================");
-            Member findMember = em.find(Member.class, member.getId());
-            System.out.println("==================");
-
-            List<AddressEntity> addressHistory = findMember.getAddressHistory();
-
-            addressHistory.stream().forEach((addressEntity) -> {
-                System.out.println(addressEntity.getAddress().getCity());
-            });
-
-
-            findMember.getFavoriteFoods().stream().forEach((favoriteFood) -> {
-                System.out.println(favoriteFood);
-            });
-
-            findMember.setHomeAddress(new Address("changeCity", findMember.getHomeAddress().getStreet(), findMember.getHomeAddress().getZipcode()));
-
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "20000"));
-            findMember.getAddressHistory().add(new AddressEntity("changeNewCity1", "street", "20000"));
-
-
+            }
             tx.commit();
         } catch (Exception e){
             tx.rollback();
