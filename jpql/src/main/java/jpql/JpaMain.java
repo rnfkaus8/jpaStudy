293b1bcaeb3aa3226c5827cdc80dbf17e6bc9ce5
rnfkaus8 <kth4350@gmail.com>
2022-03-25 07:30:04 +0900

    프로젝션 - 스칼라 타입  1. Query 타입으로 조회  2. Object[] 타입으로 조회  3. new 명령어로 조회   - 단순 값을 DTO로 조회   - 패키지 명을 포함한 전체 클래스명 입력   - 순서와 타입이 일치하는 생성자 필요

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -18,46 +18,10 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            /**
-             * 엔티티 프로젝션
-             * 해당 쿼리의 결과는 영속성 컨테스트에서 관리
-             */
-//            List<Member> result = em.createQuery("select m from Member m", Member.class)
-//                    .getResultList();
-//
-//            Member findMember = result.get(0);
-//            findMember.setAge(20);
+            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
 
-
-            /**
-             * 엔티티 프로젝션
-             * 조인쿼리가 나감  
-             * join 쿼리는 성능에 영향을 줄 가능성이 높고 튜닝할 부분이 많기때문에
-             * 해당 방법은 권장하지 않음
-             */
-//            List<Team> resultTeam = em.createQuery("select m.team from Member m", Team.class).getResultList();
-
-            /**
-             * 위와 같은 조인 쿼리가 나감
-             * 명시적으로 표현이 되어서 권장
-             */
-
-//            List<Team> resultTeam2 = em.createQuery("select t from Member m join m.team", Team.class).getResultList();
-
-
-            /**
-             * 임베디드 타입 프로젝션
-             */
-//            List<Order> orderList = em.createQuery("select o.address from Order o", Order.class).getResultList();
-
-            /**
-             * 스칼라 타입 프로젝션
-             */
-            List<Object[]> resultList = em.createQuery("select m.username, m.age from Member m").getResultList();
-
-            Object[] result = resultList.get(0);
-            System.out.println(result[0]);
-            System.out.println(result[1]);
+            MemberDTO memberDTO = resultList.get(0);
+            System.out.println(memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e){
