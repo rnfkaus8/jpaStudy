bd11a5df6605d2db5fc7614b95a63cc3496c15e8
rnfkaus8 <kth4350@gmail.com>
2022-03-28 21:26:46 +0900

    기본 case 식

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -23,22 +23,26 @@ public class JpaMain {
             member.setAge(10);
             member.setType(MemberType.ADMIN);
 
+            member.setTeam(team);
+
+
             em.persist(member);
 
 //            em.flush();
 //            em.clear();
 
-            String query = "select m.username, 'HELLO', true from Member m" +
-                    " where m.type = :userType";
 
-            List<Object[]> resultList = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
-                    .getResultList();
+            String query = "select " +
+                                "case when m.age <= 10 then '학생요금' " +
+                                     "when m.age >= 60 then '경로요금' " +
+                                     "else '일반요금' " +
+                                     "end " +
+                                "from Member m";
+            List<String> resultList = em.createQuery(query, String.class).getResultList();
 
-            for (Object[] objects : resultList) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[1] = " + objects[1]);
-                System.out.println("objects[2] = " + objects[2]);
+            for (String s : resultList) {
+                System.out.println("s = " + s);
+                
             }
 
 
