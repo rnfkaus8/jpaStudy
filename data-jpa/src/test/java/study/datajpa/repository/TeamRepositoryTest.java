f7b8ed17caf2bdd467802664f18f705018e3ce7a
rnfkaus8 <kth4350@gmail.com>
2022-04-20 20:59:15 +0900

    Team 기본 CRUD 생성 및 테스트 코드 작성

---
+++ data-jpa/src/test/java/study/datajpa/repository/TeamRepositoryTest.java
@@ -0,0 +1,68 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class TeamRepositoryTest {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Test
+    void saveAndFindById() throws Exception{
+        //given
+        Team teamA = new Team("teamA");
+        teamRepository.save(teamA);
+        //when
+        Team findTeam = teamRepository.findById(teamA.getId()).orElseThrow();
+        //then
+        assertThat(findTeam).isEqualTo(teamA);
+    }
+
+    @Test
+    void delete() throws Exception{
+        //given
+        Team teamA = new Team("teamA");
+        teamRepository.save(teamA);
+        //when
+        assertThat(teamRepository.count()).isEqualTo(1);
+        //then
+        teamRepository.delete(teamA);
+        assertThat(teamRepository.count()).isEqualTo(0);
+    }
+    
+    @Test
+    void findAllAndCount() throws Exception{
+        //given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        //when
+        List<Team> findTeams = teamRepository.findAll();
+        long count = teamRepository.count();
+        //then
+        assertThat(findTeams.size()).isEqualTo(2);
+        assertThat(findTeams).hasSameElementsAs(Arrays.asList(teamA, teamB));
+        assertThat(count).isEqualTo(2);
+
+    }
+
+
+
+}
