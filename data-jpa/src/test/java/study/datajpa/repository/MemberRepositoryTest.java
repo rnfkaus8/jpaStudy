460a6985adeb7fef15f04446357bc43d865a9f82
rnfkaus8 <kth4350@gmail.com>
2022-04-27 11:43:21 +0900

    Spring Data JPA 벌크성 수정쿼리  - @Modifying 어노테이션을 사용하지 않으면 org.hibernate.hql.internal.QueryExecutionRequestException: Not supported for DML operations 오류 발생  - 벌크성 쿼리를 실행하고 나서 영속성 컨텍스트 초기화: @Modifying(clearAutomatically = true)

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -235,4 +235,23 @@ class MemberRepositoryTest {
         assertThat(page.hasNext()).isTrue();
     }
 
+    @Test
+    void bulkAgePlus() throws Exception{
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+
+        //when
+        int resultCount = memberRepository.bulkAgePlus(20);
+        List<Member> findMembers = memberRepository.findByUsername("member5");
+        Member member = findMembers.get(0);
+
+        //then
+        assertThat(resultCount).isEqualTo(3);
+        assertThat(member.getAge()).isEqualTo(41);
+    }
+
 }
