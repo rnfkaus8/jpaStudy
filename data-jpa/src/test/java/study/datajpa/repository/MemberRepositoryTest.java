a80853777c8a86ce253c2b4b8699c2cf38ef8976
rnfkaus8 <kth4350@gmail.com>
2022-04-28 14:31:07 +0900

    QueryHint  - org.springframework.data.jpa.repository.QueryHints 어노테이션을 사용  - forCounting : 반환 타입으로 Page 인터페이스를 적용하면 추가로 호출하는 페이징을 위한 count 쿼리도 쿼리 힌트 적용(기본값 true )

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -287,4 +287,20 @@ class MemberRepositoryTest {
 
     }
 
+    @Test
+    void queryHint(){
+        //given
+        Member member1 = new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findReadOnlyByUsername(member1.getUsername());
+        findMember.setUsername("member2");
+
+        em.flush();
+
+    }
+
 }
