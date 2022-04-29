6702f5fc783aa42e361df3f1910054e9006b4857
rnfkaus8 <kth4350@gmail.com>
2022-04-29 20:49:22 +0900

    Spring Data JPA로 auditing 해결  - @EnableJpaAuditing : 스프링 부트 설정 클래스에 적용  - @CreatedDate : 등록시간  - @LastModifiedDate : 수정시간  - @CreatedBy : 등록자  - @LastModifiedBy : 수정자  - AuditorAware : 등록자, 수정자를 처리해주는 클래스, Bean으로 등록, 실무에서는 세션 정보나, 스프링 시큐리티 로그인 정보에서 ID를 받음

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -14,7 +14,7 @@ import static javax.persistence.FetchType.LAZY;
         name = "Member.findByUsername",
         query = "select m from Member m where username = :username"
 )
-public class Member extends JpaBaseEntity {
+public class Member extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
