1d3e35b5ec26e0c8ff5585217490bdde068d1e4f
rnfkaus8 <kth4350@gmail.com>
2022-05-04 13:01:07 +0900

    결과 조회  - fetch() : 리스트 반환, 데이터가 없을 시 빈 리스트 반환  - fetchOne() : 단 건 조회, 결과가 없으면 null, 2개 이상이면 com.querydsl.core.NonUniqueResultException  - fetchFirst() : .limit(1).fetchOne()  - fetchResults() : 페이징 정보 포함, total count 쿼리 추가 실행   -> groupby having등 복잡한 쿼리 실행 시 오류 발생, 미지원 예정   -> fetch(), count 쿼리 별도 실행 필요

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,8 @@ import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.*;
@@ -95,5 +98,28 @@ public class QuerydslBasicTest {
                 .fetchOne();
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void resultFetch(){
+//        List<Member> fetch = queryFactory
+//                .selectFrom(member)
+//                .fetch();
+//
+//        Member fetchOne = queryFactory
+//                .selectFrom(member)
+//                .fetchOne();
+//
+//        Member fetchFirst = queryFactory
+//                .selectFrom(member)
+//                .fetchFirst();
+
+        QueryResults<Member> fetchResults = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        fetchResults.getTotal();
+        List<Member> results = fetchResults.getResults();
+
     }
 }
